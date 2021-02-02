3e9351505271393719f4015267f2b74e18e320cb
FelixB <45665838+FelixLoot@users.noreply.github.com>
2021-02-02 13:40:26 +0100

    Fixed Articels
    + Started Styling Boxes
    + Fixed Implementing Music

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3330;
-
+const fs = require('fs')
 
 app.set('view engine', 'ejs');
+
 app.use(express.static('public'))
 app.use(express.static('views'))
 
@@ -11,7 +12,35 @@ app.use(express.static('views'))
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
-  
+
+app.get('/free', (req,res) => {
+    let info = []
+    let files = fs.readdirSync('./public/articels/free')
+    files.forEach(Element => {
+        let fileInfo = fs.readFileSync(`./public/articels/free/${Element}`)
+        let student = JSON.parse(fileInfo)
+        info.push(student)
+    })
+
+    res.render('free-dw.ejs', {array: info} )
+})
+
+app.get('/premium', (req,res) => {
+    let info = []
+    let files = fs.readdirSync('./public/articels/premium')
+    files.forEach(Element => {
+        let fileInfo = fs.readFileSync(`./public/articels/premium/${Element}`)
+        let student = JSON.parse(fileInfo)
+        info.push(student)
+    })
+
+    res.render('premium-dw.ejs', {array: info} )
+})
+
+
+
+app.get('/articels', (req,res) => {
+})
 
 
 
